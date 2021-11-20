59ec72ff86e7637fddac374b460e6f4cdccd8f2e
Diego Kleiman <kleiman.diego@gmail.com>
2021-11-20 12:44:41 -0300

    hechos los parcials menos head, agregado controllers y corregido el nav

--- app.js
+++ app.js
@@ -3,35 +3,11 @@ const app = express();
 
 const path = require("path");
 
-const route = require("./routes/main-routers.js");
+const mainRoute = require("./routes/main-routers.js");
 
 app.set("view engine", "ejs");
 
-app.use("/", route); // req es la / y el res es route
-
-// res.sendFile(path.join(__dirname, "/views/index.html")); esto lo va a hacer el metodo Home del main-controller
-
-// app.use("/login", route);
-
-// app.get("/login", function (req, res) {
-//   res.sendFile(path.join(__dirname, "/views/login.html"));
-// });
-
-// app.get("/productCart", function (req, res) {
-//   res.sendFile(path.join(__dirname, "/views/productCart.html"));
-// });
-
-// app.get("/productDetail", function (req, res) {
-//   res.sendFile(path.join(__dirname, "/views/productDetail.html"));
-// });
-
-// app.get("/registerClient", function (req, res) {
-//   res.sendFile(path.join(__dirname, "/views/registerClient.html"));
-// });
-
-// app.get("/registerProf", function (req, res) {
-//   res.sendFile(path.join(__dirname, "/views/registerProf.html"));
-// });
+app.use("/", mainRoute); // req es la "/" y el res es el main-router, el encargado de redirigir
 
 app.listen(3000, function () {
   console.log("servidor activo");
