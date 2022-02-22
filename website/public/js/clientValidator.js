77cc811f7351002c65ef1767348c6d6a0dcdcd7e
Diego Kleiman <kleiman.diego@gmail.com>
2022-02-21 21:57:27 -0300

    Terminado validacion RegisterClient FrontEnd

---
+++ website/public/js/clientValidator.js
@@ -0,0 +1,99 @@
+window.addEventListener("load", function () {
+  const formulario = document.getElementById("clientform");
+
+  formulario.addEventListener("submit", function (e) {
+    let errors = [];
+
+    const firstName = document.querySelector("#firstName");
+    const reName = new RegExp(/[^a-z]/);
+
+    if (
+      firstName.value == "" ||
+      firstName.value.length < 3 ||
+      firstName.value.match(reName) != null //sera nulo solo cuando haya letras
+    ) {
+      errors.push("tu Nombre debe contener al menos 2 letras");
+    }
+    const lastName = document.querySelector("#lastName");
+
+    if (
+      lastName.value == "" ||
+      lastName.value.length < 3 ||
+      lastName.value.match(reName) != null
+    ) {
+      errors.push("tu Apellido debe contener al menos 2 letras");
+    }
+    const email = document.querySelector("#email");
+
+    const reEmail = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+
+    if (email.value == "" || email.value.match(reEmail) == null) {
+      errors.push("Debes ingresar un Email valido");
+    }
+
+    const reNumber = new RegExp(/[^0-9]/);
+    if (
+      mobile.value == "" ||
+      mobile.value.length < 8 ||
+      mobile.value.match(reNumber) != null
+    ) {
+      errors.push("Debes ingresar un telefono valido");
+    }
+
+    const city_Id = document.querySelector("#city_Id");
+
+    if (city_Id.value == "") {
+      errors.push("Debes seleccionar una Ciudad");
+    }
+
+    const address = document.querySelector("#address");
+    const reAddress = new RegExp(/[^A-Za-z0-9\s]/);
+    console.log(address.value);
+
+    if (address.value == "" || address.value.match(reAddress) != null) {
+      errors.push("la direccion debe ser valida, ej: Calle falsa 123");
+    }
+
+    const dni = document.querySelector("#dni");
+    const reDni = new RegExp(/[^0-9]/);
+
+    if (
+      dni.value == "" ||
+      dni.value.length < 8 ||
+      dni.value.match(reDni) != null
+    ) {
+      errors.push("DNI sin espacios, puntos ni guiones");
+    }
+
+    const role = document.querySelector("#role");
+
+    if (role.value == "") {
+      errors.push("Debes seleccionar tu perfil (cliente o profesional)");
+    }
+
+    const password = document.querySelector("#password");
+    const password2 = document.querySelector("#password2");
+    const rePassword = new RegExp(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/
+    );
+
+    if (password.value.match(rePassword) == null) {
+      errors.push(
+        "la contraseña debe tener al menos 8 caracteres, una mayus, un numero,  y algun: ! @ # $ % ^ & * "
+      );
+    }
+    if (password2.value != password.value) {
+      errors.push("las contraseñas deben Coincidir ");
+    }
+
+    if (errors.length > 0) {
+      e.preventDefault();
+
+      let ulErrors = document.querySelector("#prueba");
+
+      for (i = 0; i < errors.length; i++) {
+        ulErrors.innerHTML += "<li>" + errors[i] + "</li>";
+      }
+    }
+  });
+});
--- website/public/js/validationFrontEnd.js
+++ website/public/js/validationFrontEnd.js
@@ -1,131 +1,116 @@
+window.addEventListener("load", function () {
+  const formulario = document.getElementById("prof-form");
 
+  formulario.addEventListener("submit", function (e) {
+    let errors = [];
 
+    const professionId = document.querySelector("#professionId");
 
+    if (professionId.value == "") {
+      errors.push("Debe elegir una Profesión");
+    }
 
-window.addEventListener("load", function() {
+    //
+    const re = new RegExp(/[0-9]/);
 
+    const otherJob = document.querySelector("#otherJob");
+    if (otherJob.value != "") {
+      if (otherJob.value.length <= 2) {
+        errors.push("Completa la Profesión(minimo 2 caracteres");
+      } else if (otherJob.value.match(re) != null) {
+        errors.push("La Profesion no puede tener numeros");
+      }
+    }
 
-const formulario = document.getElementById("prof-form");
+    //
 
+    //const haveLicence = document.querySelector('#have-licence');
+    var haveLicence = document.querySelector(
+      'input[name="haveLicence"]:checked'
+    );
 
- formulario.addEventListener("submit", function(e){
-    
+    var licence = document.querySelector("#licence");
 
-let errors = [];
+    if (haveLicence) {
+      if (licence.value == "") {
+        errors.push("Completa el numero de Licencia");
+      }
+    }
+    //
 
-const professionId = document.querySelector("#professionId");
+    const workZone = document.querySelector("#workZone");
 
-if (professionId.value == "") {
-errors.push("Debe elegir una Profesión")};
+    if (workZone.value == "") {
+      errors.push("Debe elegir una Zona de Trabajo");
+    }
 
+    //
 
+    //
 
-//
-const re = new RegExp(/[0-9]/);
+    const emergency = document.querySelector("#emergency");
 
-const otherJob = document.querySelector("#otherJob");
-if (otherJob.value != "") {if(otherJob.value.length <= 2 ){errors.push("Completa la Profesión(minimo 2 caracteres")}
- else if(otherJob.value.match(re) != null){errors.push("La Profesion no puede tener numeros")}
-}
+    if (emergency.value == "") {
+      errors.push("El campo emergencia no puede estar vacio");
+    }
 
-//
+    //
 
-//const haveLicence = document.querySelector('#have-licence');         
-var haveLicence = document.querySelector('input[name="haveLicence"]:checked');
+    //
+    var workDays = document.querySelector('input[name="dayShift"]:checked');
+    //const workDays = document.querySelector("#workDays:checked");
 
-var licence = document.querySelector("#licence");
+    if (!workDays) {
+      errors.push("Debe elegir un horarios de Trabajo");
+    }
 
-if (haveLicence) {if(licence.value ==""){errors.push("Completa el numero de Licencia")}};
-//
+    //
 
-const workZone = document.querySelector("#workZone");
+    //
 
-if (workZone.value == "") {
-    errors.push("Debe elegir una Zona de Trabajo")};
+    const whyMe = document.querySelector("#whyMe");
 
+    if (whyMe.value == "") {
+      errors.push("Debe completar el campo Razones para elegirme");
+    } else if (whyMe.value.length < 50 || whyMe.value.length > 500) {
+      errors.push("El mensaje debe contener entre 50 y 500 letras");
+    }
 
-//
+    //
 
+    //
 
-//
+    const price = document.querySelector("#price");
 
-const emergency = document.querySelector("#emergency");
+    if (price.value == "") {
+      errors.push("Debe completar el campo Precio");
+    } else if (whyMe.value.isNumber == false) {
+      errors.push("El Precio solo puede contener numeros");
+    }
 
-if (emergency.value == "") {
-    errors.push("El campo emergencia no puede estar vacio")};
+    //
 
+    //
 
-//
+    const cbu = document.querySelector("#cbu");
 
-//
-var workDays = document.querySelector('input[name="dayShift"]:checked')
-//const workDays = document.querySelector("#workDays:checked");   
+    if (cbu.value == "") {
+      errors.push("Debe completar el CBU");
+    } else if (cbu.value.isNumber == false) {
+      errors.push("El CBU solo puede contener numeros");
+    } else if (cbu.value.length != 22) {
+      errors.push("El CBU debe contener 22 numeros");
+    }
+    //
 
-if (!workDays) {
-    errors.push("Debe elegir un horarios de Trabajo")};
+    if (errors.length > 0) {
+      e.preventDefault();
 
+      let ulErrors = document.querySelector("#prueba");
 
-//
-
-//
-
-const whyMe = document.querySelector("#whyMe");
-
-if (whyMe.value == "") {
-    errors.push("Debe completar el campo Razones para elegirme")}
-    else if (whyMe.value.length < 50 || whyMe.value.length > 500  ) {errors.push("El mensaje debe contener entre 50 y 500 letras")}
-    
-    
-    
-    ;
-
-
-//
-
-//
-
-const price = document.querySelector("#price");
-
-if (price.value == "") {
-    errors.push("Debe completar el campo Precio")}
-    else if (whyMe.value.isNumber == false ) {errors.push("El Precio solo puede contener numeros")}
-    
-    
-    
-    ;
-
-
-//
-
-//
-
-const cbu = document.querySelector("#cbu");
-
-if (cbu.value == "") {
-    errors.push("Debe completar el CBU")}
-    else if (cbu.value.isNumber == false ) {errors.push("El CBU solo puede contener numeros")}
-    
-    else if (cbu.value.length != 22  ) {errors.push("El CBU debe contener 22 numeros")}   
-    ;
-
-
-//
-
-
-
-
-;
-
-if (errors.length > 0){e.preventDefault();
-
-
-let ulErrors = document.querySelector("#prueba");
-
-
-for(i=0 ; i < errors.length; i++){
-
-ulErrors.innerHTML += "<li>" + errors[i] + "</li>"}
-
-
-
-}})})
+      for (i = 0; i < errors.length; i++) {
+        ulErrors.innerHTML += "<li>" + errors[i] + "</li>";
+      }
+    }
+  });
+});
